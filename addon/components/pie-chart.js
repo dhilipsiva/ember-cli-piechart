0a54f0135dff4cb522448718f5ee7fb7d0f9dd08
dhilipsiva <dhilipsiva@gmail.com>
2016-10-04 16:12:08 +0530

    The simplest working pie-chart

--- addon/components/pie-chart.js
+++ addon/components/pie-chart.js
@@ -1,13 +1,58 @@
 import Ember from 'ember';
-import layout from '../templates/components/pie-chart';
 
 export default Ember.Component.extend({
-  layout,
-  data: null,
-  width: 100,
-  height: 100,
-  tagName: 'canvas',
+  data: null, // This is an array of {"value", "color"}
+  width: 100, // Default width of canvas
+  height: 100, // Default height of canvas
+  tagName: 'canvas', // This component by default takes `canvas data`
+  attributeBindings: ['width', 'height'],
   setupPieChart: Ember.on('didInsertElement', function() {
-    alert("Loading")
+    var ctx = this.element.getContext("2d"),
+      data = this.get("data"),
+      width = this.get("width"),
+      height = this.get("height"),
+      fullAngle = Math.PI * 2, // i.e 360 Deg
+      lastAngle = - Math.PI / 2, // This is 12'o clock. `0` Deg 3'0 clock
+      currentAngle = 0, // This angle respective of the current value
+      totalValue = 0, // Cumulative total of `value` in data
+      center_x = width / 2, // Horizontal center of canvas
+      center_y = height / 2, // Vertical center of canvas
+      radius = 0, // Radius (Duh!)
+      i = 0,
+      l = 0,
+      item = null;
+
+    /*
+     * Let the smallest center be the radius to comtain the entire pie chart
+     * within the canvas
+     */
+    if (center_x < center_y) {
+      radius = center_x;
+    }
+    else {
+      radius = center_y;
+    }
+
+    // Calculate totalValue
+    for (i = 0, l = data.length; i < l; i++) {
+      item = data[i];
+      totalValue += item.value;
+    }
+
+    for (i = 0, l = data.length; i < l; i++) {
+      /*
+       * Draw each pie
+       */
+      item = data[i];
+      currentAngle = fullAngle * (item.value / totalValue);
+
+      ctx.beginPath();
+      ctx.fillStyle = item.color;
+      ctx.arc(center_x, center_y, radius, lastAngle, lastAngle + currentAngle);
+      ctx.lineTo(center_x, center_y);
+      ctx.fill();
+
+      lastAngle += currentAngle;
+    }
   })
 });
--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return [
+      {"value": 1, "color": "red"},
+      {"value": 4.5, "color": "blue"},
+      {"value": 2, "color": "yellow"},
+      {"value": 3, "color": "green"}
+    ];
+  }
 });
